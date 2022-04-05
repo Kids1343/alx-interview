90650fa3a6d98a7d81840058f06f72c5450b71c2
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-04-05 07:36:29 +0300

    Create 0-pascal_triangle.py

---
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+"""
+    0-pascal_triangle.py: pascal_triangle()
+"""
+
+
+def generate(self, numRows: int) -> List[List[int]]:
+    """
+            returns a lis of lists of integers
+            Args:
+                numRows (int): number of lists and digits
+            Returns: list of lists
+        """
+    res = [[1]]
+
+    for i in range(numRows - 1):
+        temp = [0] + res[-1] + [0]
+        rows = []
+        for j in range(len(res) + 1):
+            rows.append(temp[j] + temp[j + 1])
+        res.append(rows)
+    return res
+
+
