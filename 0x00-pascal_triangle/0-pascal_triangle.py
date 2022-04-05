6b97852c0438d8383bdbd1646ea4e7d76caa9b6a
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-04-05 07:53:31 +0300

    Update 0-pascal_triangle.py

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -5,16 +5,22 @@
 """
 
 
-def pascal_triangle(numRows: int) -> List[List[int]]:
+def def pascal_triangle(n): 
     """
             returns a lis of lists of integers
             Args:
                 numRows (int): number of lists and digits
             Returns: list of lists
         """
+    if n <= 0:
+        # return empty list
+       return []
+     
     res = [[1]]
+    if n == 1:
+        return res
 
-    for i in range(numRows - 1):
+    for i in range(n - 1):
         temp = [0] + res[-1] + [0]
         rows = []
         for j in range(len(res) + 1):
