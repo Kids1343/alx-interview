10f27e37329835536e49c0255ffbaf89d9001995
Kids1343 <84654265+Kids1343@users.noreply.github.com>
2022-06-10 23:36:02 +0300

    Update 0-island_perimeter.py

--- 0x09-island_perimeter/0-island_perimeter.py
+++ 0x09-island_perimeter/0-island_perimeter.py
@@ -1,21 +1,22 @@
 #!/usr/bin/python3
-"""returns the perimeter of the island described in grid"""
+"""5-island_perimeter module"""
 
 
-def islandPerimeter(grid):
-        """
-        :type grid: List[List[int]]
-        :rtype: int
-        """
-        count, repeat = 0, 0
+def island_perimeter(grid):
+    """Returns the perimeter of the island described in grid"""
+    c = 0
+    length = len(grid) - 1
+    width = len(grid[0]) - 1
 
-        for i in xrange(len(grid)):
-            for j in xrange(len(grid[i])):
-                if grid[i][j] == 1:
-                    count += 1
-                    if i != 0 and grid[i - 1][j] == 1:
-                        repeat += 1
-                    if j != 0 and grid[i][j - 1] == 1:
-                        repeat += 1
-
-        return 4*count - 2*repeat
+    for i, r in enumerate(grid):
+        for j, n in enumerate(r):
+            if n == 1:
+                if i == 0 or grid[i - 1][j] != 1:
+                    c += 1
+                if j == 0 or grid[i][j - 1] != 1:
+                    c += 1
+                if j == width or grid[i][j + 1] != 1:
+                    c += 1
+                if i == length or grid[i + 1][j] != 1:
+                    c += 1
+    return c
